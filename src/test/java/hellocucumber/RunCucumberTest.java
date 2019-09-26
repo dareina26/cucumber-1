b240eef94087c4868baae4a1dcf2aff68d0125dd
diego.reina <diego.reina.garzon@gmail.com>
2019-09-26 11:21:04 -0500

    Ejemplo Hola mundo finalizado

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -1,5 +1,9 @@
 package hellocucumber;
 
+import cucumber.api.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -7,4 +11,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", features = "src/test/resources/hellocucumber")
 public class RunCucumberTest {
+
+
 }
